2bdc7dc49dfe78e04cb5c73dfdc29aff4f04f429
samcaspus <sandeepguptan1998@gmail.com>
2021-11-20 21:27:02 +0530

    learning

--- main.go
+++ main.go
@@ -4,4 +4,12 @@ import "fmt"
 
 func main(){
 	fmt.Println("hello how are you");
+	fmt.Println("variables can be used as follows");
+
+	var number int = 10;
+	fmt.Println(number)
+
+	var name string= "sandeep";
+	fmt.Println(name)
+
 }
