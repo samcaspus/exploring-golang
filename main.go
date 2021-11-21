69ad8b2802369f7a8aba620c962e34944bfb3ddf
samcaspus <sandeepguptan1998@gmail.com>
2021-11-21 20:56:42 +0530

    learning input output and addition for integers in string

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func variables(){
@@ -12,7 +13,6 @@ func variables(){
 	fmt.Println("variables can be used as follows");
 	var number int = 10;
 	fmt.Println(number)
-
 	var name string= "sandeep";
 	fmt.Println(name)
 }
@@ -22,10 +22,23 @@ func inputOutput(){
 	input, _ := reader.ReadString('\n');
 	value, _ := strconv.Atoi(input)
 	fmt.Println(value);
+}
 
+func additionOfTwoNumbers(){
+	/// will also try if else / conditonal statements here
+	reader := bufio.NewReader(os.Stdin);
+
+	number1, _ := reader.ReadString('\n');
+	num, err := strconv.ParseFloat(strings.TrimSpace(number1), 64);
+	if err != nil {
+		fmt.Println(err);
+
+	}
+	fmt.Println(num + 1);
 }
 
 func main(){
 	variables();
 	inputOutput();
+	additionOfTwoNumbers();
 }
