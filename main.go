2e8c4ee24f25e0cd945f91aa968413be85ef9ffa
samcaspus <sandeepguptan1998@gmail.com>
2021-11-21 20:45:48 +0530

    learning input output in golang for string

--- main.go
+++ main.go
@@ -1,15 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
-func main(){
+func variables(){
 	fmt.Println("hello how are you");
 	fmt.Println("variables can be used as follows");
-
 	var number int = 10;
 	fmt.Println(number)
 
 	var name string= "sandeep";
 	fmt.Println(name)
+}
 
+func inputOutput(){
+	reader := bufio.NewReader(os.Stdin);
+	input, _ := reader.ReadString('\n');
+	value, _ := strconv.Atoi(input)
+	fmt.Println(value);
+
+}
+
+func main(){
+	variables();
+	inputOutput();
 }
