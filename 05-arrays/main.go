caecc3556d54a37c99c15beae37be1e98af11ebe
samcaspus <sandeepguptan1998@gmail.com>
2022-01-04 22:12:27 +0530

    progress 7

---
+++ 05-arrays/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	fmt.Println("Arrays")
+
+	var fruits [6]string;
+	fruits[0] = "1"
+	fruits[1] = "2"
+	fruits[2] = "3"
+	fruits[3] = "4"
+	fruits[5] = "6"
+	
+	
+	fmt.Println(fruits, len(fruits))
+	
+
+}
