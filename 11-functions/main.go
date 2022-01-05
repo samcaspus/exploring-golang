ac3616b0049515de743d921f0ca75b48529e1c97
samcaspus <sandeepguptan1998@gmail.com>
2022-01-05 21:53:49 +0530

    progress 14

--- 11-functions/main.go
+++ 11-functions/main.go
@@ -6,8 +6,30 @@ func main(){
 
 	fmt.Println("functions in golang")
 	greeter()
+
+	sandeep := User{"sandeep",23,true}
+	fmt.Println(sandeep)
+	sandeep.getStatus()
+	sandeep.getAge()
 }
 
 func greeter(){
 	fmt.Println("this is greeter")
+}
+
+
+type User struct{
+	Name string
+	Age int
+	Status bool
+}
+
+
+func (user User) getStatus(){
+	fmt.Println(user.Status)
+}
+
+
+func (user User) getAge(){
+	fmt.Println(user.Age)
 }
